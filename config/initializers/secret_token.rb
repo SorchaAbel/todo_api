3a78046775af81d5c74ae6594cb0aac15bc360b4
Sorcha Abel <Sorcha.Abel@htw.com.au>
2016-06-18 18:29:46 +1000

    simple api using jsonapi resources

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoJsonApi::Application.config.secret_key_base = 'c83cd6da06da48fa9e8454d6633d2c65427043f83b73bc85c332fc123652e0755c7871eed1553a116fabe8f5a973220f3c8682cfe570395eae9ccfd376552aa1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TodoJsonApi::Application.config.session_store :cookie_store, key: '_todo-json-api_session'
